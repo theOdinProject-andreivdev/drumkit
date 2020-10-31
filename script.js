8e5e87df69df01c5e6dc264d37ee959ef76fa1dc
Andrei Voinea <andreivdev@gmail.com>
2020-10-31 12:58:58 +0200

    Added click events as well. Mobile friendly

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const keys = document.querySelectorAll(`.key`);
 
-window.addEventListener("keydown", playSound);
+window.addEventListener("keydown", playSoundKeypress);
 
 keys.forEach((key) => {
+  key.addEventListener("click", playSoundClick);
   key.addEventListener("transitionend", removeTransition);
 });
 
-function playSound(e) {
+function playSoundKeypress(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+
+  key.classList.add("playing");
+}
+
+function playSoundClick(e) {
+  const audio = document.querySelector(`audio[data-key="${this.dataset.key}"]`);
+  const key = document.querySelector(`.key[data-key="${this.dataset.key}"]`);
 
   if (!audio) return;
 
